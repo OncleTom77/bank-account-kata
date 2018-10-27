97217452b3d768c8e3dfe991a365e4ff02ca8a3b
thomasfouan <thomasfouan@hotmail.fr>
2018-10-27 11:52:57 +0200

    refactor test

--- src/test/java/com/kata/bankaccount/AccountTest.java
+++ src/test/java/com/kata/bankaccount/AccountTest.java
@@ -10,29 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(JUnitParamsRunner.class)
 public class AccountTest {
 
-	@Parameters({"0", "1", "2", "3", "7", "10"})
+	@Parameters({"0, 1", "0, 7", "2, 0", "8, 0", "1, 2", "2, 2", "3, 2", "9, 4", "6, 15"})
 	@Test
-	public void should_get_deposit_amount_when_balance_amount_is_0(int depositAmount) {
-		Account account = new Account(0);
-
-		int balance = account.deposit(depositAmount);
-
-		assertThat(balance).isEqualTo(depositAmount);
-	}
-
-	@Parameters({"0", "1", "2", "5", "9", "15"})
-	@Test
-	public void should_get_balance_amount_when_deposit_amount_is_0(int balanceAmount) {
-		Account account = new Account(balanceAmount);
-
-		int balance = account.deposit(0);
-
-		assertThat(balance).isEqualTo(balanceAmount);
-	}
-
-	@Parameters({"1, 1", "1, 2", "2, 2", "3, 2", "9, 4", "6, 15"})
-	@Test
-	public void should_add_deposit_amount_to_balance_amount_when_amounts_are_positives(int balanceAmount, int depositAmount) {
+	public void should_deposit_amount_to_balance_amount(int balanceAmount, int depositAmount) {
 		Account account = new Account(balanceAmount);
 
 		int balance = account.deposit(depositAmount);
