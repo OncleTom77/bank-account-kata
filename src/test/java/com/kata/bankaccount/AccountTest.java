8f74e1c3ef8a7fd4dd59ea18e1c8e452d6466d96
thomasfouan <thomasfouan@hotmail.fr>
2018-10-27 13:19:19 +0200

    refactor test for deposit in Account

--- src/test/java/com/kata/bankaccount/AccountTest.java
+++ src/test/java/com/kata/bankaccount/AccountTest.java
@@ -1,6 +1,7 @@
 package com.kata.bankaccount;
 
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,31 +12,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(JUnitParamsRunner.class)
 public class AccountTest {
 
+	@Parameters({
+			"0, 0, 0",
+			"0, 1, 1", "0, 3, 3",
+			"1, 0, 1", "3, 0, 3",
+			"5, 3, 8", "2, 9, 11"
+	})
 	@Test
-	public void should_update_account_balance_when_deposit_positive_amount() {
-		Account account = Account.of(0);
+	public void should_update_account_balance_when_deposit_positive_amount(int balanceAmount, int depositAmount, int expectedAmount) {
+		Account account = Account.of(balanceAmount);
 
-		account.deposit(1);
+		account.deposit(depositAmount);
 
-		assertThat(account).isEqualTo(Account.of(1));
-	}
-
-	@Test
-	public void should_update_account_balance_when_deposit_positive_amount_2() {
-		Account account = Account.of(0);
-
-		account.deposit(2);
-
-		assertThat(account).isEqualTo(Account.of(2));
-	}
-
-	@Test
-	public void should_update_account_balance_when_deposit_positive_amount_3() {
-		Account account = Account.of(1);
-
-		account.deposit(1);
-
-		assertThat(account).isEqualTo(Account.of(2));
+		assertThat(account).isEqualTo(Account.of(expectedAmount));
 	}
 
 	@Test
